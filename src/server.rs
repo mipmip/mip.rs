4dabe69456e1d89ba3f14d7dd388053d5153ceec
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2026-05-08 18:57:28 -0400

    Handles temp files

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,20 @@
+use warp::Filter;
+
 pub struct RestBro;
 
 impl RestBro {
 
-    pub async fn run_bro(path_dir: &'static str, port: u16) {
+    pub async fn run_bro(path_dir: &'static str, temp_dir: &'static str, port: u16) {
 
         println!("{}", path_dir);
-        warp::serve(warp::fs::dir(path_dir))
+
+        let temp_html = warp::path(".temp.html")
+            .and(warp::fs::file(format!("{}/.temp.html", temp_dir)));
+        let temp_seed = warp::path(".temp.seed")
+            .and(warp::fs::file(format!("{}/.temp.seed", temp_dir)));
+        let assets = warp::fs::dir(path_dir);
+
+        warp::serve(temp_html.or(temp_seed).or(assets))
             .run(([127, 0, 0, 1], port))
             .await;
     }
